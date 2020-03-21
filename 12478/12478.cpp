a77df0ba785683773e6e3eee2c370492451c56ef
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-03-21 14:13:55 +0430

    Create 12478.cpp

---
+++ 12478/12478.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+//
+using namespace std;
+typedef long long int ll;
+typedef long long unsigned int llu;
+//
+int main()
+{
+	cout << "KABIR" << endl;
+}
