e3763fb95fd807e86cc9143c5b8680e9b2239f80
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-03-21 11:09:05 +0430

    Create 10106.cpp

---
+++ 10106/10106.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+//
+using namespace std;
+typedef long long int ll;
+typedef long long unsigned int llu;
+//
+void ssum(string a, string b, char arr[])
+{
+	if (b.length() > a.length())
+	{
+		string d;
+		d = a;
+		a = b;
+		b = d;
+	}
+	int sum = 0;
+	int counter = 0;
+	for (int i = a.length() - 1; i >= 0; i--)
+	{
+		sum += a[i] - '0';
+		if (counter < b.length())
+		{
+			sum += b[b.length() - counter - 1] - '0';
+		}
+		arr[counter] = '0' + (sum % 10);
+		counter++;
+		sum /= 10;
+	}
+	while (sum > 0)
+	{
+		arr[counter] = '0' + sum % 10;
+		sum /= 10;
+		counter++;
+	}
+	arr[counter] = '\0';
+	char arr3[11000];
+	int zd = 0;
+	for (int i = counter - 1; i >= 0; i--)
+	{
+		arr3[zd] = arr[i];
+		zd++;
+	}
+	arr3[counter] = '\0';
+	for (size_t i = 0; i < counter; i++)
+	{
+		arr[i] = arr3[i];
+	}
+	arr[counter] = '\0';
+}
+//
+void zero(string a, int nz, char end[])
+{
+	for (size_t i = 0; i < a.length(); i++)
+	{
+		end[i] = a[i];
+	}
+	for (size_t i = a.length(); i < a.length() + nz; i++)
+	{
+		end[i] = '0';
+	}
+	end[nz + a.length()] = '\0';
+}
+//
+void mult(string a, string b, char c[])
+{
+	llu sum = 0;
+	char main[100000] = "0";
+	for (size_t i = 0; i < b.length(); i++)
+	{
+		int digit = b[i] - '0';
+		for (size_t j = 0; j < digit; j++)
+		{
+			char zeroo[10000];
+			zero(a, b.length() - i - 1, zeroo);
+
+			string a1 = zeroo;
+			/*for (size_t z = 0; z < b.length() - i; z++)
+			{
+				ddd
+					//zone
+				a1[z + a.length()] = '0';
+			}*/
+			string hmain = main;
+			ssum(hmain, a1, main);
+			//cout << main << endl;
+		}
+	}
+	int size = 0;
+	for (size = 0; main[size]; size++);
+	for (size_t i = 0; i < size; i++)
+	{
+		c[i] = main[i];
+	}
+	c[size] = '\0';
+}
+//
+void itostr(char str[], int number)
+{
+	//str.clear();
+	int d = number;
+	int len = 0;
+	for (len = 0; d > 0; len++)
+	{
+		d /= 10;
+	}
+	char rev[10000];
+	for (size_t i = 0; i < len; i++)
+	{
+		rev[i] = number % 10 + '0';
+		number /= 10;
+	}
+	rev[len] = '\0';
+	int index = 0;
+	char newc[100000];
+	for (int i = len - 1; i >= 0; i--)
+	{
+		newc[index] = rev[i];
+		index++;
+	}
+	newc[index] = '\0';
+	for (size_t i = 0; i < len; i++)
+	{
+		str[i] = newc[i];
+	}
+	str[len] = '\0';
+
+}
+//
+int digitl(int a)
+{
+	int i;
+	if (a == 0)
+	{
+		return 1;
+	}
+	for (i = 0; a > 0; i++)
+	{
+		a /= 10;
+	}
+	return i;
+}
+//
+int main()
+{
+	string a, b;
+	while (cin >> a >> b)
+	{
+		char zarb[100000];
+		mult(a, b, zarb);
+		cout << zarb << endl;
+	}
+}
