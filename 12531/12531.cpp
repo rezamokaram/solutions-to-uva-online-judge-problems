784ed5eaacaaa67ccf6ac2436a261782dbe49a43
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-03-21 14:15:17 +0430

    Create 12531.cpp

---
+++ 12531/12531.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <functional>
+#include <map>
+#include <set>
+//
+using namespace std;
+typedef long long int ll;
+typedef long long unsigned int llu;
+//
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n%6==0)
+		{
+			cout << "Y" << endl;
+		}
+		else
+		{
+			cout << "N" << endl;
+		}
+	}
+}
