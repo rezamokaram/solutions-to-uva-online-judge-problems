3309f70a402b4258e9fc10b36a0fdfb29966ee0a
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-03-21 14:18:43 +0430

    Create 12708.cpp

---
+++ 12708/12708.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <functional>
+#include <map>
+#include <set>
+//
+using namespace std;
+typedef long long int ll;
+typedef long long unsigned int llu;
+//
+int main()
+{
+	ll tc;
+	llu n;
+	cin >> tc;
+	while (tc--)
+	{
+		cin >> n;
+		cout << n / 2 << endl;
+	}
+}
