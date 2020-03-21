afda2a564ddbea65f4f29dc189c2e6cb0a0bc71a
Reza Mokaram <60672244+RezaMokaram@users.noreply.github.com>
2020-03-21 14:20:47 +0430

    Create 13025.cpp

---
+++ 13025/13025.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <functional>
+#include <map>
+#include <set>
+//
+using namespace std;
+typedef long long int ll;
+typedef long long unsigned int llu;
+//
+int main()
+{
+	cout << "May 29, 2013 Wednesday" << endl;
+}
