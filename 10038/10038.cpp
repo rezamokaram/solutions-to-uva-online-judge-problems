062a5b39e4877ee8162b8e03115e1a3cbca28c3b
Reza Mokaram <rezamokaramdost@gmail.com>
2023-12-08 23:42:00 +0330

    Update 10038.cpp

--- 10038/10038.cpp
+++ 10038/10038.cpp
@@ -1,59 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector> 
+#include <iterator>
 #include <algorithm>
+
 using namespace std;
-void sortt(int list[], int size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		for (size_t j = 0; j < i; j++)
-		{
-			if (list[i]<list[j])
-			{
-				int temp = list[i];
-				list[i] = list[j];
-				list[j] = temp;
-			}
-		}
-	}
-}
-int cheker(int list[],int size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		if (list[i]!=i+1)
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
+const int maxn = 30000 + 100;
 int main()
 {
-	int a;
-	while (cin >> a)
-	{
-		int arr[10000],ary[10000];
-		for (size_t i = 0; i < a; i++)
-		{
-			cin >> arr[i];
-		}
-		for (size_t i = 0; i < (a - 1); i++)
-		{
-			ary[i] = arr[i] - arr[i + 1];
-			if (ary[i]<0)
-			{
-				ary[i] = ary[i]*(-1);
-			}
-		}
-		sortt(ary,a-1);
-		if (cheker(ary,a-1)==1)
-		{
-			cout << "Not jolly" << endl;
-		}
-		else
-		{
-			cout << "Jolly" << endl;
-		}
-	}
-	system("pause>A");
+  int a,n,b,B[maxn], A[maxn];
+  bool ch;
+    
+  while(cin>>n)
+  {
+    int tmp = a;
+    ch = false;
+    for(int i=0;i<n;i++)
+    {
+        cin>>B[i];
+    }
+    for(int i=0;i<n-1;i++){
+        A[i] = (abs(B[i+1]-B[i]));
+    }
+    sort(A, A+n-1);
+    for(int i=0;i<n-1;i++){
+        if(A[i] != i+1) {
+            ch = true;
+            break;
+        }
+    }
+    if(ch==true){
+       cout<<"Not jolly\n";
+    }
+    else if(ch==false){
+       cout<<"Jolly\n";
+    }
+  }
+  return 0;
 }
