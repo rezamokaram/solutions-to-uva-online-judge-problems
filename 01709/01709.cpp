5428e44c0c03dbb246eeea33e014e2df22295ab1
Reza Mokaram <rezamokaramdost@gmail.com>
2025-04-04 03:48:27 +0330

    Create 01709.cpp

---
+++ 01709/01709.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int p, a, b, c, d, n;
+    while (scanf("%d %d %d %d %d", &p, &a, &b, &c, &d) == 5) {
+        scanf("%d", &n);
+        double max_decline = 0;
+        double max = p * (sin((a + b)) + cos((c + d)) + 2);
+        for (int k = 2; k <= n; k++) {
+            double curr = p * (sin((a * k + b)) + cos((c * k + d)) + 2);
+            if (max > curr) {
+                if(max_decline< max - curr){
+                    max_decline=max-curr;
+                }
+            } else {
+                max = curr;
+            }
+        }
+        printf("%.8lf\n", max_decline);
+    }
+    return 0;
+}
